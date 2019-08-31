25af0ec34c63decda0fe87810ee65a8bdae0ad03
solisandrew117 <solisandrew117@cs104u-3d.acad.fullerton.edu>
2019-08-31 16:13:40 -0700

    Bill

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,2 +1,30 @@
-// Name: 
+// Name:
 // This program calculates the tax and tip on a restaurant bill.
+
+#include <iostream>
+
+int main()
+{
+  double tax, tip, total_bill, total_meal_cost;
+
+  // Welcome to the Restaurant Bill Calculator.
+  std::cout << "Welcome to the Restaurant Bill Calculator!\n ";
+  std::cout << "What is the 'total meal cost'? ";
+  std::cin >> total_meal_cost;
+
+  // Adding tax to total cost.
+  tax = 0.0775 * total_meal_cost;
+  std::cout << "tax:        " << tax << "\n";
+
+  // Adding tip to total cost.
+  tip = 0.20 * total_meal_cost;
+  std::cout << "tip:        " << tip << "\n";
+
+  // Calculate total meal cost, tax, and tip.
+  total_bill = total_meal_cost + tax + tip;
+
+  // The total bill cost.
+  std::cout << "The \"total bill\": " << total_bill << ". \n";
+
+  return 0;
+}
